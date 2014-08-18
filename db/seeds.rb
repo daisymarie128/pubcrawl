d3ef3431e1f3a83096ddc325e41a8538e6525e54
daisymarie <daisy.m.s@hotmail.com>
2014-08-18 19:45:17 +1000

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,4 @@ PubChallenge.destroy_all
 Pub.destroy_all
 
 u1 = User.create(:frist_name => 'u1', :last_name => 'u1last', :username => 'u1', :avatar => 'http://placekitten.com/500/500', :email => 'user@gmail.com', :location => 'Sydney', :password => 'p', :password_confirmation => 'p', :score => '100')
+c1 = PubChallenge.create(:name => 'c1', :image => 'c1', :image_cache => 'c1', :description => 'c1', :badge => 'c1')
