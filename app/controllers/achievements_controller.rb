6ebde3b0bd78077621610d3b5694411ee784c16a
daisymarie <daisy.m.s@hotmail.com>
2014-08-21 23:53:05 +1000

    fixing deployment

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,44 +1,44 @@
-# class AchievementsController < ApplicationController
+class AchievementsController < ApplicationController
 
 
-#   def index
-#     @achievements = @current_user.achievements
-#     render :json => @achievement
-#   end
+  def index
+    @achievements = @current_user.achievements
+    render :json => @achievement
+  end
 
-#   def new
-#     @achievement = Achievement.new
-#     render :json => @achievement
-#   end
+  def new
+    @achievement = Achievement.new
+    render :json => @achievement
+  end
 
-#   #create a new achievment and add it to the current user
-#   #check then set the acheivments pub challenge id to the current pub challenge
-#   def create
-#     @achievement = Achievement.new achievement_params
-#     @achievement.user = @current_user.id
-#     @achievement.pub_challenge = params[:id]
-#     if @achievement.save
-#       # render :json => @achievement #{ status: 'ok'}
-#     else
-#       render :json => {:errors => @achievement.errors}
-#     end
-#   end
+  #create a new achievment and add it to the current user
+  #check then set the acheivments pub challenge id to the current pub challenge
+  def create
+    @achievement = Achievement.new achievement_params
+    @achievement.user = @current_user.id
+    @achievement.pub_challenge = params[:id]
+    if @achievement.save
+      # render :json => @achievement #{ status: 'ok'}
+    else
+      render :json => {:errors => @achievement.errors}
+    end
+  end
 
-#   def show
-#     @achievement = Achievement.find params[:id]
-#     render :json => @achievement
+  def show
+    @achievement = Achievement.find params[:id]
+    render :json => @achievement
 
-#   end
+  end
 
-#   def destroy
-#     @achievement = Achievement.find params[:id]
-#     @achievement.destroy
-#   end
+  def destroy
+    @achievement = Achievement.find params[:id]
+    @achievement.destroy
+  end
 
-#   private
-#     def achievement_params
-#     params.require(:achievement).permit(:pub_challenges)
-#     end
-#   end
+  private
+    def achievement_params
+    params.require(:achievement).permit(:pub_challenges)
+    end
 
-# end
+
+end
