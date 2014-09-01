da7a52e91bcd1d325793e283bd9dbb6ee2dec982
daisymarie <daisy.m.s@hotmail.com>
2014-09-01 20:30:00 +1000

    deleted etag that broke things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # before_action :cors_preflight_check
-  after_action :cors_set_headers
+  before_action :cors_preflight_check
+  before_action :cors_set_headers
   skip_before_action :verify_authenticity_token
   protect_from_forgery with: :exception
   before_action :authenticate_user
@@ -21,7 +21,6 @@ class ApplicationController < ActionController::Base
 
   def cors_set_headers
     headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Expose-Headers'] = 'ETag'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
