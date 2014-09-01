05026155bed3adceded41fa7e89df4a44a8fea30
lyntco <strawww@gmail.com>
2014-09-01 20:49:03 +1000

    Testing config only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  after_action :cors_set_headers
-  protect_from_forgery with: :exception
+  # after_action :cors_set_headers
   skip_before_action :verify_authenticity_token
+  protect_from_forgery with: :exception
   before_action :authenticate_user
 
   private
@@ -18,11 +18,11 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def cors_set_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  end
+  # def cors_set_headers
+  #   headers['Access-Control-Allow-Origin'] = '*'
+  #   headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+  #   headers['Access-Control-Request-Method'] = '*'
+  #   headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  # end
 
 end
