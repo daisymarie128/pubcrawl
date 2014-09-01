c8ffaf3850ca4bdb4730fe35c4ede8532680bfd8
lyntco <strawww@gmail.com>
2014-09-01 23:15:44 +1000

    testing forgery protect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+  before_action :cors_preflight_check
+  after_action :cors_set_access_control_headers
+
   skip_before_action :verify_authenticity_token
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
   before_action :authenticate_user
 
   private
@@ -14,6 +18,23 @@ class ApplicationController < ActionController::Base
 
     if @current_user.nil?
       session[:user_id] = nil
+    end
+  end
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH,OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :content_type => 'text/plain'
     end
   end
 
