9691c6d72201cd8be6c355e325de3230d7a5b480
lyntco <strawww@gmail.com>
2014-09-01 23:28:50 +1000

    But we need sercurrity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
-  # before_action :cors_preflight_check
-  # after_action :cors_set_access_control_headers
-
   skip_before_action :verify_authenticity_token
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
   before_action :authenticate_user
 
   private
@@ -20,22 +17,5 @@ class ApplicationController < ActionController::Base
       session[:user_id] = nil
     end
   end
-
-  # def cors_set_access_control_headers
-  #   headers['Access-Control-Allow-Origin'] = '*'
-  #   headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH,OPTIONS'
-  #   headers['Access-Control-Max-Age'] = "1728000"
-  # end
-
-
-  # def cors_preflight_check
-  #   if request.method == :options
-  #     headers['Access-Control-Allow-Origin'] = '*'
-  #     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, OPTIONS'
-  #     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
-  #     headers['Access-Control-Max-Age'] = '1728000'
-  #     render :text => '', :content_type => 'text/plain'
-  #   end
-  # end
 
 end
